225d2ea18dc5ecdf15ddc5e422603c45d09f15da
Braden Walker <bradencodes@gmail.com>
2019-03-24 08:33:00 -0700

    peel fully responsive to pixels

--- src/components/Peel.js
+++ src/components/Peel.js
@@ -3,26 +3,28 @@ import React, { Component } from 'react';
 class Peel extends Component {
   render() {
     return (
-      <div id="Awesome" className="anim750">
-        
-        <div className="reveal circle_wrapper">
-          <div className="circle">Hello!</div>
-        </div>
-                  
-        <div className="sticky anim750">
-          <div className="front circle_wrapper anim750">
-            <div className="circle darkout anim750"></div>
+      <div className="peel_container">
+        <div id="Awesome" className="anim750">
+          
+          <div className="reveal circle_wrapper">
+            <div className="circle">Hello!</div>
           </div>
-        </div>
-        
-        <h4>Peel Me!</h4>
-                  
-        <div className="sticky anim750">
-          <div className="back circle_wrapper anim750">
-            <div className="circle darkout anim750"></div>
+                    
+          <div className="sticky anim750">
+            <div className="front circle_wrapper anim750">
+              <div className="circle darkout anim750"></div>
+            </div>
           </div>
+          
+          <h4>Peel Me!</h4>
+                    
+          <div className="sticky anim750">
+            <div className="back circle_wrapper anim750">
+              <div className="circle darkout anim750"></div>
+            </div>
+          </div>
+                    
         </div>
-                  
       </div>
     );
   }
