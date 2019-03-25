b737f4b5132e83140ee4d1c0cbf4310dfe13ada7
Braden Walker <bradencodes@gmail.com>
2019-03-25 10:14:21 -0700

    init turn component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
+import $ from "jquery";
 import './styles/css/index.css';
+
 import Peel from './components/Peel.js';
+import Turn from './components/Turn.js';
+
+const options = {
+  width: 800,
+  height: 600,
+  autoCenter: false,
+  display: "double",
+  acceleration: true,
+  elevation: 50,
+  gradients: !$.isTouch,
+  when: {
+    turned: function(e, page) {
+      console.log("Current view: ", $(this).turn("view"));
+    }
+  }
+};
+
+const pages = [
+  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/01.jpg",
+  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/02.jpg"
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Peel />
+        <Turn options={options} className="magazine">
+          {pages.map((page, index) => (
+            <div key={index} className="page">
+              <img src={page} alt="" />
+            </div>
+          ))}
+        </Turn>
       </div>
     );
   }
